febd66dba3e309c776b4f7075d2e532c4ad66dc3
Ahmed7313 <ahmedrabie7313@gmail.com>
2023-10-16 08:01:41 -0700

    added checking condetion for handling wrong data type used in teh function

--- printf.c
+++ printf.c
@@ -3,41 +3,44 @@
 /**
  * _printf - Our custom printf function.
  * @format: The format string.
- * @... : The values to format and print.
+ * @...: The values to format and print.
  * Return: The number of characters printed.
  */
 int _printf(const char *format, ...)
 {
-    unsigned int i = 0, count = 0;
-    va_list args;
+	unsigned int i = 0, count = 0;
+	va_list args;
 
-    va_start(args, format);
-    while (format && format[i])
-    {
-        if (format[i] == '%' && (format[i + 1] == 'c' ||
-                                 format[i + 1] == 's' || format[i + 1] == '%'))
-        {
-            switch (format[i + 1])
-            {
-                case 'c':
-                    count += print_char(args);
-                    break;
-                case 's':
-                    count += print_string(args);
-                    break;
-                case '%':
-                    count += print_percent(args);
-                    break;
-            }
-            i++;
-        }
-        else
-        {
-            write(1, &format[i], 1);
-            count++;
-        }
-        i++;
-    }
-    va_end(args);
-    return (count);
+	va_start(args, format);
+	while (format && format[i])
+	{
+		if (format[i] == '%' &&
+		    (format[i + 1] == 'c' || format[i + 1] == 's' ||
+		     format[i + 1] == '%'))
+		{
+			switch (format[i + 1])
+			{
+				case 'c':
+					count += print_char(args);
+					break;
+				case 's':
+					count += print_string(args);
+					break;
+				case '%':
+					count += print_percent();
+					break;
+				default:
+					return (-1);
+			}
+			i++;
+		}
+		else
+		{
+			write(1, &format[i], 1);
+			count++;
+		}
+		i++;
+	}
+	va_end(args);
+	return (count);
 }
