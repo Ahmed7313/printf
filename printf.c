8adfd953d87b3dc9449c6f5fd8c9cae595c378f1
Ahmed7313 <ahmedrabie7313@gmail.com>
2023-10-17 09:20:16 -0700

    adhere to betty

--- printf.c
+++ printf.c
@@ -11,29 +11,29 @@
  */
 int _printf(const char *format, ...)
 {
-        unsigned int i = 0, count = 0;
-        va_list args;
+	unsigned int i = 0, count = 0;
+	va_list args;
 
-        if (format == NULL)
-                return (-1);
+	if (format == NULL)
+		return (-1);
 
-        va_start(args, format);
-        while (format && format[i])
-        {
-                if (format[i] == '%' && format[i + 1] == '\0')
-                        return (-1);
-                else if (format[i] == '%' && format[i + 1] == '%')
-                {
-                        count += handle_unformat('%');
-                        i += 2;
-                        continue;
-                }
-                else if (format[i] == '%')
-                        count += handle_format(format, &i, args);
-                else
-                        count += handle_unformat(format[i]);
-                i++;
-        }
-        va_end(args);
-        return ((count == 0) ? -1 : (int)count);
+	va_start(args, format);
+	while (format && format[i])
+	{
+		if (format[i] == '%' && format[i + 1] == '\0')
+			return (-1);
+		else if (format[i] == '%' && format[i + 1] == '%')
+		{
+			count += handle_unformat('%');
+			i += 2;
+			continue;
+		}
+		else if (format[i] == '%')
+			count += handle_format(format, &i, args);
+		else
+			count += handle_unformat(format[i]);
+		i++;
+	}
+	va_end(args);
+	return ((count == 0) ? -1 : (int)count);
 }
