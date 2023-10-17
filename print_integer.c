db616cdd245e21ff83d06bbfa8d48b03959ef94f
Nariman Khaled <nariman.khaled.mohamed@gmail.com>
2023-10-17 21:26:52 +0300

    new print int

--- print_integer.c
+++ print_integer.c
@@ -1,46 +1,47 @@
 #include "main.h"
 
 /**
-* print_intger - Prints an intger.
-* @args: The va_list of arguments.
-*
-* Return: count of characters typed
-*/
-int print_intger(va_list args)
-{
+ * print_num_rec - prints an integer recursively
+ * @n: Integer
+ */
 
-	int n = va_arg(args, int);
-	int number, tenth, count = 0, size = 1;
+void print_num_rec(int n)
+{
+	unsigned int num = n;
 	char digit;
 
-	if (n < 0)
+	if (num / 10)
+		print_num_rec(num / 10);
+
+	digit = '0' + num % 10;
+	write(1, &digit, 1);
+}
+
+/**
+ * print_intger - prints an integer
+ * @vl: Integer
+ * Return: int
+ */
+
+int print_intger(va_list vl)
+{
+	unsigned int n = va_arg(vl, int);
+	int count = 1, num = n;
+	char digit;
+
+	if (num < 0)
 	{
-		char minus = '-';
-
-		count++;
-		write(1, &minus, 1);
-		number = n * -1;
-	}
-	else
-	{
-		number = n;
-	}
-
-	tenth = number;
-
-	while (tenth > 9)
-	{
-		tenth /= 10;
-		size *= 10;
-	}
-
-	for ( ; size >= 1; size /= 10)
-	{
-		count++;
-		digit = (char) (((number / size) % 10) + '0');
-
+		digit = '-';
 		write(1, &digit, 1);
+		num *= -1;
+		n = num;
+		count += 1;
 	}
 
+	while (n > 9)
+		n /= 10,
+			count++;
+
+	print_num_rec(num);
 	return (count);
 }
