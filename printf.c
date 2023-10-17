7e4b67b443b53c8e731f381700a3c73fa1aaf7ad
Ahmed7313 <ahmedrabie7313@gmail.com>
2023-10-17 05:59:02 -0700

    test old function

--- printf.c
+++ printf.c
@@ -4,37 +4,6 @@
 #include <unistd.h>
 
 /**
- * handle_format - Handles format specifiers and returns character count.
- * @format: The format string.
- * @i: Pointer to index where '%' was found.
- * @args: List of arguments.
- * Return: Number of printed characters.
- */
-unsigned int handle_format(const char *format, unsigned int *i, va_list args)
-{
-	unsigned int count = 0;
-
-	if (format[*i + 1] == '%')
-	{
-		write(1, "%", 1);
-		count++;
-		*i += 2;
-	}
-	else if (format[*i + 1] == 'c' || format[*i + 1] == 's')
-	{
-		(*i)++;
-		count += (format[*i] == 'c') ? print_char(args) : print_str(args);
-	}
-	else
-	{
-		write(1, &format[*i], 2);
-		count += 2;
-		*i += 2;
-	}
-	return (count);
-}
-
-/**
  * _printf - Our custom printf function.
  * @format: The format string.
  * @... : The values to format and print.
@@ -47,20 +16,43 @@ int _printf(const char *format, ...)
 
 	if (format == NULL)
 		return (-1);
+
 	va_start(args, format);
 	while (format && format[i])
 	{
-		if (format[i] == '%' && format[i + 1] == '\0')
-			return (-1);
-		if (format[i] == '%')
-			count += handle_format(format, &i, args);
+		if (format[i] == '%' && format[i + 1] == '\0') /* Add this condition */
+	{
+		return (-1);
+	}
+	else if (format[i] == '%' && format[i + 1] == '%')
+	{
+		write(1, "%", 1);
+		count++;
+		i += 2;
+		continue;
+	}
+		else if (format[i] == '%' &&
+			 (format[i + 1] == 'c' || format[i + 1] == 's'))
+		{
+			i++;
+			count += (format[i] == 'c') ? print_char(args) : print_str(args);
+		}
+		else if (format[i] == '%' &&
+			 format[i + 1] != 'c' && format[i + 1] != 's' &&
+			 format[i + 1] != '%')
+		{
+			write(1, &format[i], 2);
+			count += 2;
+			i += 2;
+			continue;
+		}
 		else
 		{
 			write(1, &format[i], 1);
 			count++;
-			i++;
 		}
+		i++;
 	}
 	va_end(args);
-	return ((count == 0) ? -1 : (int)count);
+	return ((count == 0) ? -1 : count);
 }
