beee3b07ae69e06cc84cd9d8200dd1524d792db9
Ahmed7313 <ahmedrabie7313@gmail.com>
2023-10-17 02:31:57 -0700

    Solved merging errores and added handling for NULL cases

--- main.h
+++ main.h
@@ -3,17 +3,15 @@
 
 #include <stdarg.h>
 #include <unistd.h>
+#include <stddef.h>
 
 /* Function Prototypes */
 int _printf(const char *format, ...);
 int print_char(va_list args);
-<<<<<<< HEAD
 int print_str(va_list args);
-=======
 int print_string(va_list args);
 int print_percent(va_list args);
 int print_intger(va_list args);
 int print_Binary(va_list args);
->>>>>>> a295d327ad74284d6aceb8398601c907616b26da
 
 #endif /* MAIN_H */
--- printf.c
+++ printf.c
@@ -1,5 +1,7 @@
 #include "main.h"
 #include <stdarg.h>
+#include <stddef.h>
+#include <unistd.h>
 
 /**
  * _printf - Our custom printf function.
@@ -16,10 +18,8 @@ int _printf(const char *format, ...)
 		return (-1);
 
 	va_start(args, format);
-
 	while (format && format[i])
 	{
-<<<<<<< HEAD
 		if (format[i] == '%' && (format[i + 1] == 'c' || format[i + 1] == 's'))
 		{
 			i++;
@@ -32,35 +32,6 @@ int _printf(const char *format, ...)
 		{
 			write(1, "invalid", 7);
 			return (-1);
-=======
-		if (format[i] == '%' &&
-		    (format[i + 1] == 'c' || format[i + 1] == 's' ||
-		     format[i + 1] == '%' || format[i + 1] == 'd' ||
-			 format[i + 1] == 'i' || format[i + 1] == 'b'))
-		{
-			switch (format[i + 1])
-			{
-				case 'c':
-					count += print_char(args);
-					break;
-				case 's':
-					count += print_string(args);
-					break;
-				case '%':
-					count += print_percent(args);
-					break;
-				case 'i':
-				case 'd':
-					count += print_intger(args);
-					break;
-				case 'b':
-					count += print_Binary(args);
-					break;
-				default:
-					return (-1);
-			}
-			i += 2;
->>>>>>> a295d327ad74284d6aceb8398601c907616b26da
 		}
 		else
 		{
@@ -70,7 +41,6 @@ int _printf(const char *format, ...)
 		i++;
 	}
 	va_end(args);
-
 	if (count == 0)
 		return (-1);
 
