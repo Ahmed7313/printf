f56464cd4c429a18370985f4f91a9d6a8c9a2096
Nariman Khaled <nariman.khaled.mohamed@gmail.com>
2023-10-17 19:43:13 +0300

    print unsigned hex

---
+++ print_unsHex.c
@@ -0,0 +1,40 @@
+#include "main.h"
+
+/**
+ * print_unsignedHex - Prints an unsigned hex.
+ * @args: The va_list of arguments.
+ *
+ * Return: count of characters typed
+ */
+int print_unsignedHex(va_list args)
+{
+	unsigned int number, tmp_number;
+	int count = 0, size = -1;
+	int j;
+	int tmpArr[128];
+	char digit;
+
+	number = va_arg(args, unsigned int);
+	tmp_number = number;
+
+	while (tmp_number > 16)
+	{
+		size++;
+		tmpArr[size] = tmp_number % 16;
+		tmp_number /= 16;
+	}
+
+	if (tmp_number >= 0 && tmp_number <= 15)
+	{
+		size++;
+		tmpArr[size] = tmp_number;
+	}
+	for (j = size - 1; size >= 0; size--)
+	{
+		count++;
+		digit = (char)((tmpArr[size]) + '0');
+		write(1, &digit, 1);
+	}
+
+	return (count);
+}
