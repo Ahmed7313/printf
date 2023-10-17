9cb71479e2a243199cc953c4e09bfa248228ae65
Ahmed7313 <ahmedrabie7313@gmail.com>
2023-10-17 09:14:23 -0700

    old fun

--- printf.c
+++ printf.c
@@ -7,31 +7,50 @@
  * _printf - Our custom printf function.
  * @format: The format string.
  * @... : The values to format and print.
- * Return: The number of characters printed or -1 on error.
+ * Return: The number of characters printed, or -1 on error.
  */
 int _printf(const char *format, ...)
 {
 	unsigned int i = 0, count = 0;
 	va_list args;
 
-	if (!format || (format[i] == '%' && format[i + 1] == '\0'))
+	if (format == NULL)
 		return (-1);
+
 	va_start(args, format);
 	while (format && format[i])
 	{
-		if (format[i] == '%')
+		if (format[i] == '%' && format[i + 1] == '\0') /* Add this condition */
+		{
+			return (-1);
+		}
+		else if (format[i] == '%' && format[i + 1] == '%')
+		{
+			write(1, "%", 1);
+			count++;
+			i += 2;
+			continue;
+		}
+		else if (format[i] == '%' &&
+			 (format[i + 1] == 'c' || format[i + 1] == 's'))
 		{
 			i++;
-			if (format[i] == 'c' || format[i] == 's' || format[i] == '%')
-				count += handle_format(format, &i, args);
-			else
-			{
-				count += handle_unformat('%');
-				count += handle_unformat(format[i]);
-			}
+			count += (format[i] == 'c') ? print_char(args) : print_str(args);
+		}
+		else if (format[i] == '%' &&
+			 format[i + 1] != 'c' && format[i + 1] != 's' &&
+			 format[i + 1] != '%')
+		{
+			write(1, &format[i], 2);
+			count += 2;
+			i += 2;
+			continue;
 		}
 		else
-			count += handle_unformat(format[i]);
+		{
+			write(1, &format[i], 1);
+			count++;
+		}
 		i++;
 	}
 	va_end(args);
