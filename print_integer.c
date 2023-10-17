5c88ce2c5ba714a86df9f468e536c548aeac8abc
Nariman Khaled <nariman.khaled.mohamed@gmail.com>
2023-10-17 22:49:37 +0300

    add helper functions to print int

--- print_integer.c
+++ print_integer.c
@@ -1,6 +1,29 @@
 #include "main.h"
 
 /**
+ * writeDigit- write the digit
+ * @digit: the digit to write
+ */
+void writeDigit(char digit)
+{
+	write(1, &digit, 1);
+}
+
+/**
+ * printInt- helper recursion function
+ * @number: input number
+ */
+void printInt(int number)
+{
+	unsigned int n = number;
+
+	if (n / 10)
+		printInt(n / 10);
+
+	writeDigit('0' + n % 10);
+}
+
+/**
  * print_intger - print integer for specifier i , d
  * @args: argument to print
  *
@@ -8,41 +31,23 @@
  */
 int print_intger(va_list args)
 {
-	int n = va_arg(args, int);
-	int num, last, exp = 1;
-	char digit;
-	int i = 1;
+	unsigned int n = va_arg(args, int);
+	int count = 1, num = n;
 
-	if (n < 0)
+	if (num < 0)
 	{
-		digit = '-';
-		write(1, &digit, 1);
-		n = -n;
-		i++;
+		writeDigit('-');
+		num *= -1;
+		n = num;
+		count += 1;
 	}
 
-	last = n % 10;
-	n = n / 10;
-	num = n;
-
-	if (num > 0)
+	while (n > 9)
 	{
-		while (num / 10 != 0)
-		{
-			exp = exp * 10;
-			num = num / 10;
-		}
-		num = n;
-		while (exp > 0)
-		{
-			digit = (num / exp + '0');
-			write(1, &digit, 1);
-			num = num - (digit * exp);
-			exp = exp / 10;
-			i++;
-		}
+		n /= 10;
+		count++;
 	}
-	digit = (last + '0');
-	write(1, &digit, 1);
-	return (i);
+
+	printInt(num);
+	return (count);
 }
