edceef0e5200bb8574ff7f3bb1891cdf4f7db16e
Ahmed7313 <ahmedrabie7313@gmail.com>
2023-10-23 16:07:26 -0700

    qq

--- handle_specifier.c
+++ handle_specifier.c
@@ -1,58 +1,61 @@
 #include "main.h"
+#include <stdarg.h>
+#include <unistd.h>
+#include <stdlib.h>
 
 /**
- * _strlen - Returns the length of a string.
+ * _strlen - Calculate the length of a string.
  * @s: The string.
  * Return: The length of the string.
  */
 int _strlen(char *s)
 {
-	int length = 0;
+	int len = 0;
 
-	while (*s++)
-		length++;
-	return (length);
+	while (s[len])
+		len++;
+	return (len);
 }
 
 /**
- * int_to_str - Converts an integer to a string.
+ * int_to_str - Convert an integer to a string.
  * @num: The integer.
  * Return: The converted string.
  */
 char *int_to_str(int num)
 {
 	char *str;
-	int temp, digits = 0;
+	int i = 0, temp = num, len = 0;
 
-	temp = num;
-	while (temp)
+	while (temp != 0)
 	{
-		digits++;
+		len++;
 		temp /= 10;
 	}
 
-	str = malloc(digits + 1);
+	str = malloc(len + 1);
 	if (!str)
 		return (NULL);
 
-	str[digits] = '\0';
-	while (digits--)
+	str[len] = '\0';
+	while (num != 0)
 	{
-		str[digits] = (num % 10) + '0';
+		str[len - 1 - i] = (num % 10) + '0';
 		num /= 10;
+		i++;
 	}
 
 	return (str);
 }
 
 /**
- * write_str - Writes a string to stdout.
+ * write_str - Write a string to stdout.
  * @str: The string.
  * Return: The number of characters written.
  */
 int write_str(char *str)
 {
-	return (write(1, str, str_len(str)));
+	return (write(1, str, _strlen(str)));
 }
 
 /**
@@ -71,27 +74,24 @@ int handle_specifier(char c, va_list args)
 	{
 	case 'c':
 		ch = (char)va_arg(args, int);
-		write(1, &ch, 1);
-		count++;
+		count += write(1, &ch, 1);
 		break;
 	case 's':
 		str = va_arg(args, char *);
-		if (str == NULL)
-			str = "(null)";
-		count += write(1, str, _strlen(str));
+		if (str)
+			count += write_str(str);
 		break;
 	case 'd':
 	case 'i':
 		str = int_to_str(va_arg(args, int));
 		if (str)
 		{
-			count += write(1, str, _strlen(str));
+			count += write_str(str);
 			free(str);
 		}
 		break;
 	case '%':
-		write(1, &c, 1);
-		count++;
+		count += write(1, &c, 1);
 		break;
 	default:
 		break;
